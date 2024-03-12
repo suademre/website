4fc3e7d5254b45e839598f560a3bafcfa62acbf9
suademre <suadeumar@gmail.com>
2024-03-12 23:46:56 +0100

    Add filtered products by price-in progress

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -7,6 +7,8 @@ import { useShoppingContext } from "../../context/ShoppingContext";
 import useShopping from "../../hooks/useShopping";
 import { useFavoritesContext } from "../../context/FavoritesContext";
 import useFavorites from "../../hooks/useFavorites";
+import { useState } from "react";
+import { IProducts } from "../../types";
 
 const Categories = () => {
   const { data: products } = useFetchData("products");
@@ -24,22 +26,85 @@ const Categories = () => {
   );
   console.log("cateogryProducts: ", categoryProducts);
 
+  //price ranges
+  interface IPriceRanges {
+    id: number | string;
+    min: number;
+    max: number;
+  }
+  const priceRanges = [
+    { id: 1, label: "0 - 100", min: 0, max: 100 },
+    { id: 2, label: "101 - 200", min: 101, max: 200 },
+    { id: 3, label: "201 - 300", min: 201, max: 300 },
+    { id: 4, label: "301 - 400", min: 301, max: 400 },
+    { id: 5, label: "401 - 500", min: 301, max: 500 },
+    // Diğer aralıklar...
+  ];
+
+  // Başlangıçta hiçbir aralık seçilmemiş
+  const [selectedPriceRanges, setSelectedPriceRanges] = useState<
+    (number | string)[]
+  >([]);
+
+  const handlePriceRangeChange = (aralikId: number | string) => {
+    setSelectedPriceRanges((current) =>
+      current.includes(aralikId)
+        ? current.filter((id) => id !== aralikId)
+        : [...current, aralikId]
+    );
+  };
+
+  const filteredProductsByPrice = (
+    products: IProducts[],
+    selectedPriceRanges: (number | string)[]
+  ): IProducts[] => {
+    if (selectedPriceRanges.length === 0) return products;
+    return products.filter((product) =>
+      selectedPriceRanges.some((rangesId) => {
+        const aralik = priceRanges.find((aralik) => aralik.id === rangesId);
+        if (!aralik) return false;
+        return product.price >= aralik.min && product.price <= aralik.max;
+      })
+    );
+  };
+
   return (
-    <Container>
-      {categoryProducts.map((product, index) => (
-        <Card
-          key={index}
-          imageFrond={product.imageFrond}
-          title={product.title}
-          price={product.price}
-          // onAdd={() => toggleShoppingItem(product.id)}
-          onAddShopping={() => handleShoppings(product)}
-          onAddFavorites={() => handleFavorites(product)}
-          isInShoppingList={shopping.some((item) => item.id === product.id)}
-          isInFavoritesList={favorite.some((item) => item.id === product.id)}
-        />
-      ))}
-    </Container>
+    <>
+      <div>
+        {priceRanges.map((aralik) => (
+          <div key={aralik.id}>
+            <input
+              type="checkbox"
+              checked={selectedPriceRanges.includes(aralik.id)}
+              onChange={() => handlePriceRangeChange(aralik.id)}
+            />
+            {aralik.label}
+          </div>
+        ))}
+
+        <Container>
+          {filteredProductsByPrice(categoryProducts, selectedPriceRanges).map(
+            (product, index) => (
+              <Card
+                key={index}
+                imageFrond={product.imageFrond}
+                title={product.title}
+                price={product.price}
+                // onAdd={() => toggleShoppingItem(product.id)}
+                onAddShopping={() => handleShoppings(product)}
+                onAddFavorites={() => handleFavorites(product)}
+                isInShoppingList={shopping.some(
+                  (item) => item.id === product.id
+                )}
+                isInFavoritesList={favorite.some(
+                  (item) => item.id === product.id
+                )}
+              />
+            )
+          )}
+        </Container>
+      </div>
+    </>
   );
 };
 
