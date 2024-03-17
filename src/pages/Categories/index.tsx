f1e227ebf9eef83a8a828a8728788948047ad676
suademre <suadeumar@gmail.com>
2024-03-17 15:40:19 +0100

    finish filtered products by price

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -1,14 +1,12 @@
 import useFetchData from "../../hooks/api/useFetchData";
 import { useParams } from "react-router-dom";
-import { Container } from "./styles";
+import { Container, Wrapper, FilteredPrice, Label } from "./styles";
 import Card from "../../components/molekuls/Card";
-// import { IProducts } from "../../types";
 import { useShoppingContext } from "../../context/ShoppingContext";
 import useShopping from "../../hooks/useShopping";
 import { useFavoritesContext } from "../../context/FavoritesContext";
 import useFavorites from "../../hooks/useFavorites";
-import { useState } from "react";
-import { IProducts } from "../../types";
+import useFilteredByPrice from "../../hooks/useFilteredByPrice";
 
 const Categories = () => {
   const { data: products } = useFetchData("products");
@@ -21,90 +19,56 @@ const Categories = () => {
   // const [shopping, setShopping] = useState<IProducts[]>([]);
   const { slug } = useParams();
 
+  const {
+    priceRanges,
+    selectedPriceRanges,
+    handlePriceRangeChange,
+    filteredProductsByPrice,
+  } = useFilteredByPrice();
+
   const categoryProducts = products.filter(
     (item) => item.category.title === slug
   );
   console.log("cateogryProducts: ", categoryProducts);
 
-  //price ranges
-  interface IPriceRanges {
-    id: number | string;
-    min: number;
-    max: number;
-  }
-  const priceRanges = [
-    { id: 1, label: "0 - 100", min: 0, max: 100 },
-    { id: 2, label: "101 - 200", min: 101, max: 200 },
-    { id: 3, label: "201 - 300", min: 201, max: 300 },
-    { id: 4, label: "301 - 400", min: 301, max: 400 },
-    { id: 5, label: "401 - 500", min: 301, max: 500 },
-    // Diğer aralıklar...
-  ];
-
-  // Başlangıçta hiçbir aralık seçilmemiş
-  const [selectedPriceRanges, setSelectedPriceRanges] = useState<
-    (number | string)[]
-  >([]);
-
-  const handlePriceRangeChange = (aralikId: number | string) => {
-    setSelectedPriceRanges((current) =>
-      current.includes(aralikId)
-        ? current.filter((id) => id !== aralikId)
-        : [...current, aralikId]
-    );
-  };
-
-  const filteredProductsByPrice = (
-    products: IProducts[],
-    selectedPriceRanges: (number | string)[]
-  ): IProducts[] => {
-    if (selectedPriceRanges.length === 0) return products;
-    return products.filter((product) =>
-      selectedPriceRanges.some((rangesId) => {
-        const aralik = priceRanges.find((aralik) => aralik.id === rangesId);
-        if (!aralik) return false;
-        return product.price >= aralik.min && product.price <= aralik.max;
-      })
-    );
-  };
-
   return (
-    <>
-      <div>
-        {priceRanges.map((aralik) => (
-          <div key={aralik.id}>
-            <input
-              type="checkbox"
-              checked={selectedPriceRanges.includes(aralik.id)}
-              onChange={() => handlePriceRangeChange(aralik.id)}
-            />
-            {aralik.label}
-          </div>
-        ))}
-
-        <Container>
-          {filteredProductsByPrice(categoryProducts, selectedPriceRanges).map(
-            (product, index) => (
-              <Card
-                key={index}
-                imageFrond={product.imageFrond}
-                title={product.title}
-                price={product.price}
-                // onAdd={() => toggleShoppingItem(product.id)}
-                onAddShopping={() => handleShoppings(product)}
-                onAddFavorites={() => handleFavorites(product)}
-                isInShoppingList={shopping.some(
-                  (item) => item.id === product.id
-                )}
-                isInFavoritesList={favorite.some(
-                  (item) => item.id === product.id
-                )}
+    <Container>
+      <FilteredPrice>
+        <h3>Filtered Products By Price</h3>
+        <div>
+          {priceRanges.map((aralik) => (
+            <Label key={aralik.id}>
+              <input
+                type="checkbox"
+                checked={selectedPriceRanges.includes(aralik.id)}
+                onChange={() => handlePriceRangeChange(aralik.id)}
               />
-            )
-          )}
-        </Container>
-      </div>
-    </>
+              <div>{aralik.label + "€"}</div>
+            </Label>
+          ))}
+        </div>
+      </FilteredPrice>
+      <Wrapper>
+        {filteredProductsByPrice(categoryProducts, selectedPriceRanges).map(
+          (product, index) => (
+            <Card
+              key={index}
+              imageFrond={product.imageFrond}
+              title={product.title}
+              price={product.price}
+              // onAdd={() => toggleShoppingItem(product.id)}
+              onAddShopping={() => handleShoppings(product)}
+              onAddFavorites={() => handleFavorites(product)}
+              isInShoppingList={shopping.some((item) => item.id === product.id)}
+              isInFavoritesList={favorite.some(
+                (item) => item.id === product.id
+              )}
+            />
+          )
+        )}
+      </Wrapper>
+      {/* </Column> */}
+    </Container>
   );
 };
 
--- src/pages/Categories/styles.tsx
+++ src/pages/Categories/styles.tsx
@@ -1,8 +1,44 @@
 import styled from "styled-components";
+import { colors } from "../../theme/variables";
 
-export const Container = styled.div`
+export const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   margin: 80px 30px 20px;
   justify-content: center;
+`;
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 200px 1fr;
+`;
+
+export const FilteredPrice = styled.div`
+  margin-top: 300px;
+  margin-left: 30px;
+`;
+
+export const Label = styled.div`
+  display: flex;
+  margin-bottom: 8px;
+  position: relative;
+  cursor: pointer;
+  &:after {
+    content: "";
+    position: absolute;
+    width: 0;
+    height: 5px;
+    bottom: 0;
+    left: 5px;
+    background-color: ${colors.lightGreen}; /* Veya tercih ettiğiniz bir renk */
+    visibility: hidden;
+    transition: all 0.3s ease-in-out;
+  }
+
+  &:hover {
+    &:after {
+      visibility: visible;
+      width: 60%;
+    }
+  }
 `;
