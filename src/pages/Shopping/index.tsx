3bc4e22f27281e7c058cce9e6e853e54d24d11be
suademre <suadeumar@gmail.com>
2024-03-19 01:53:38 +0100

    add new card view in progress

--- src/pages/Shopping/index.tsx
+++ src/pages/Shopping/index.tsx
@@ -1,32 +1,54 @@
-import { Container } from "./styles";
+import { Container, Wrapper } from "./styles";
 import { useShoppingContext } from "../../context/ShoppingContext";
 import useShopping from "../../hooks/useShopping";
 import Card from "../../components/molekuls/Card";
 import { useFavoritesContext } from "../../context/FavoritesContext";
 import useFavorites from "../../hooks/useFavorites";
+import Image from "../../components/Image";
+import Flex from "../../components/Flex";
+import Icon from "../../components/Icon";
 
 const Shopping = () => {
   const { shopping } = useShoppingContext();
   const { handleShoppings } = useShopping();
   const { favorite } = useFavoritesContext();
   const { handleFavorites } = useFavorites();
+
+  const totalPrice = shopping.reduce((total, item) => total + item.price, 0);
   return (
-    <Container>
-      {shopping.length === 0 && <p>Es gibt keine Produkte </p>}
-      {shopping.map((product, index) => (
-        <Card
-          key={index}
-          imageFrond={product.imageFrond}
-          title={product.title}
-          price={product.price}
-          // onAdd={() => toggleShoppingItem(product.id)}
-          onAddShopping={() => handleShoppings(product)}
-          onAddFavorites={() => handleFavorites(product)}
-          isInShoppingList={shopping.some((item) => item.id === product.id)}
-          isInFavoritesList={favorite.some((item) => item.id === product.id)}
-        />
-      ))}
-    </Container>
+    <>
+      <Container>
+        {shopping.length === 0 && <p>Es gibt keine Produkte </p>}
+        {shopping.map((product, index) => (
+          <Card
+            key={index}
+            imageFrond={product.imageFrond}
+            title={product.title}
+            price={product.price}
+            // onAdd={() => toggleShoppingItem(product.id)}
+            onAddShopping={() => handleShoppings(product)}
+            onAddFavorites={() => handleFavorites(product)}
+            isInShoppingList={shopping.some((item) => item.id === product.id)}
+            isInFavoritesList={favorite.some((item) => item.id === product.id)}
+          />
+        ))}
+      </Container>
+      <div>
+        {shopping.map((product, index) => (
+          <Wrapper key={index}>
+            <Image src={product.imageFrond} alt={product.title} width="100px" />
+            <Flex flexDirection="column" alignItems="center" marginLeft="10px">
+              <h3>Title</h3>
+              <div>{product.title}</div>
+            </Flex>
+            <div>
+              <Icon name="increase" />
+              <Icon name="decrease" />
+            </div>
+          </Wrapper>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/pages/Shopping/styles.tsx
+++ src/pages/Shopping/styles.tsx
@@ -6,3 +6,14 @@ export const Container = styled.div`
   margin: 80px 30px 20px;
   justify-content: center;
 `;
+
+export const Wrapper = styled.div`
+  width: 70%;
+  display: flex;
+  align-items: center;
+  height: auto;
+  padding: 30px;
+  margin: 0 auto;
+  border: 2px solid #f6f7fb;
+  border-radius: 48px;
+`;
